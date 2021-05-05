128e1dfb40908cd2d39615208542bdc09c6b9750
Jacob Stevens <J.stevensOne@gmail.com>
2021-05-04 22:27:14 -0400

    Added DB Schema

--- db/dbhelpers.js
+++ db/dbhelpers.js
@@ -1,7 +1,22 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect('mongodb://localhost/CustomerReviews', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open' , function() {
-  console.log('we are connected!')
-})
+  console.log('we are connected?!')
+});
 
+const ReviewsSchema = new mongoose.Schema({
+  product: String,
+  userName: String,
+  rating: Number,
+  title: String,
+  location: String,
+  reviewDate: Date,
+  reviewBody: String,
+  helpfulCount: Number,
+  abuseReported: Boolean
+})
