3169aa2d930860a62b74bf1a3e67104bc89c2b76
Jacob Stevens <J.stevensOne@gmail.com>
2021-05-04 23:54:37 -0400

    Finished Seeding Script

--- db/dbhelpers.js
+++ db/dbhelpers.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const faker = require('faker');
 
 mongoose.connect('mongodb://localhost/CustomerReviews', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -6,11 +7,11 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open' , function() {
-  console.log('we are connected?! WHY CNAT I CONNECT!!')
+  console.log('we are connected?!')
 });
 
-const ReviewsSchema = new mongoose.Schema({
-  product: String,
+const reviewsSchema = new mongoose.Schema({
+  productId: String,
   userName: String,
   rating: Number,
   title: String,
@@ -20,3 +21,30 @@ const ReviewsSchema = new mongoose.Schema({
   helpfulCount: Number,
   abuseReported: Boolean
 })
+
+const Review = mongoose.model('Review', reviewsSchema);
+
+let seed = () => {
+  for (let i = 0; i <= 3000; i++) {
+    let newReview = new Review({
+      productId: faker.datatype.number(100),
+      userName: faker.internet.userName(),
+      rating: faker.datatype.number(5),
+      title: faker.lorem.words(),
+      location: faker.address.country(),
+      reviewDate: faker.date.past(),
+      reviewBody: faker.lorem.sentences(),
+      helpfulCount: faker.datatype.number(2000),
+      abuseReported: faker.random.boolean()
+    })
+    newReview.save(function(err, success) {
+      if (err) {
+        console.log('error saving to the database')
+      }
+    })
+  }
+}
+
+seed();
+
+
