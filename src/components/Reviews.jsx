2c35e76f9af81e24760a400c5cc82a7716aa1716
Jacob Stevens <J.stevensOne@gmail.com>
2021-05-18 23:51:41 -0400

    more code cleanup

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -6,7 +6,7 @@ const Reviews = (props) => (
   <div>
     <ul>
       {props.reviews.map((review, i) => (
-        <div className = "review-container">
+        <div className = "review-container" key = {i}>
           <div className = "review-username">
             <img className = "review-username_image" src="user_image.png" ></img> <div className = "review-username_text"> {review.userName} </div>
           </div>
@@ -16,7 +16,7 @@ const Reviews = (props) => (
           <div className = "location-date">Reviewed in {review.location} on {moment(review.reviewDate, 'YYYY-MM-DDThh:mm:ss.sss').format('MMMM Do, YYYY')}</div>
           <div className = "review-body">{review.reviewBody}</div>
           <div className = "helpful-count"> {review.helpfulCount} people found this helpful</div>
-          <div className> <button className = "helpful-button">Helpful</button>  <span className = "abuse-text">|  Report Abuse</span> </div>
+          <div> <button className = "helpful-button">Helpful</button>  <span className = "abuse-text">|  Report Abuse</span> </div>
         </div>
       ))}
     </ul>
