f3b658cf9e7ce4e5e651132d440bd145d6a1c177
Jacob Stevens <J.stevensOne@gmail.com>
2021-06-10 19:57:39 -0400

    removed some old code, updated user image source

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Reviews from './components/Reviews.jsx';
-import Rating from './components/Rating.jsx';
 import $ from 'jquery';
 
 class App extends React.Component {
